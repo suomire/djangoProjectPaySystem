c077c16d14a515568a901f7ec0e235d6fffb9405
suomire <olllkarpushkina@gmail.con>
2021-03-25 19:55:34 +0300

    models update

--- PaymentSystem/models.py
+++ PaymentSystem/models.py
@@ -5,6 +5,11 @@ from django.db import models
 
 # Create your models here.
 
+def generate_wallet_number():
+    time_part = int(datetime.datetime.now().timestamp())
+    return int(time_part)
+
+
 class Users(models.Model):
     """
     Users model class
@@ -19,17 +24,8 @@ class Users(models.Model):
 
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
-    wallet_number = models.BigIntegerField(
-        default=10000000)  # наличие default значения позволяет вызвать функцию save без конфликтов....
+    wallet_number = models.BigIntegerField(default=generate_wallet_number(), primary_key=True)
     total = models.IntegerField(default=5000)
-
-    """
-    # overrides method in django.model class, found in stackoverflow
-    def save(self, *args, **kwargs):
-        time_part = int(datetime.datetime.now().timestamp())
-        self.wallet_number = int(time_part)
-        super(Users, self).save(*args, **kwargs)
-    """
 
     def __str__(self):
         return "{} [{}] <{}> : {}".format(self.username, self.password, self.wallet_number, self.total)
@@ -45,8 +41,8 @@ class Transaction(models.Model):
         transaction_amount          Amount of money to send
 
     """
-    sender_wallet_number = models.BigIntegerField()
-    receiver_wallet_number = models.BigIntegerField()
+    sender_wallet_number = models.ForeignKey('Users', related_name='senders_wallet', on_delete=models.RESTRICT)
+    receiver_wallet_number = models.ForeignKey(to=Users, related_name='receiver_wallet', on_delete=models.RESTRICT)
     transaction_amount = models.IntegerField()
 
     def __str__(self):
